c8c7f5ae044c41b1e1e74f8c07e757fe5915b7b2
ndalilavin <vinson.steve@student.moringaschool.com>
2022-06-19 00:40:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    for( i =0; i<array.length; i++){
+      const compliment = target - array[i];
+      for(let j = i+1; j<array.length; j++){
+        if( array[j] === compliment)return true;
+      }
+
+      }
+      //if end of array, return false
+      return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  The time complexity is o(n) hence it is linear
+  The space complexity is o(n) since the seenNumbers will grow in proportion to the size of the input array
 */
 
 /* 
   Add your pseudocode here
+  Write a function hasTargetSum that adds two arrays that add up to target
+  If the target is right, then return true and if not return false.
 */
 
 /*
   Add written explanation of your solution here
+  //The i represents one of numbers in an array and j represents another one in which the loop iterates over it to 
+  //ensure the target is met which after iteration returns true or false
 */
 
 // You can run `node index.js` to view these console logs
